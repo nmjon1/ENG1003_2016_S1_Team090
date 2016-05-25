6a622886de91d6d505daae4bc260948b9bfc40ee
tgnon1 <lenci.han@gmail.com>
2016-05-25 12:50:22 +1000

    Update mainPage.js

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -1,5 +1,18 @@
 // Code for the main app page (locations list).
-
+/* Creat function to get weather data from the cache
+    Input: weather index, date
+    Output: Weather summary */
+init();
+function init() 
+{
+    for(var index=0;index < locationWeatherCache.length();index++) {
+    locationWeatherCache.getWeatherAtIndexForDate(index, new Date(), function(index, data){
+    var summary = 'Min: ' + fToC(data.temperatureMin) + ', Max: ' + fToC(data.temperatureMax);
+    document.getElementById('weather' + index).textContent = summary;
+    document.getElementById('icon' + index).src = 'images/' + data.icon + '.png';
+    });
+   }
+}
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
 
