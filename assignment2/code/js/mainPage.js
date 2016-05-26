98f2a4a39866eee51d53214b45b0f9082fed79cc
tgnon1 <lenci.han@gmail.com>
2016-05-26 21:30:44 +1000

    Add comments

--- assignment2/code/js/mainPage.js
+++ assignment2/code/js/mainPage.js
@@ -2,18 +2,20 @@
 // Create an event listener that waits until the DOM is loaded, when the event occurs, the function init is called.
 document.addEventListener("DOMContentLoaded", init);
 
-// Creat function to get weather data from the cache.
-// Input: Weather index, date
+// Create function to get weather data from the cache.
+// Input: Location index, date
 // Output: Weather summary
 function init() 
 {
     for(var index=0;index < locationWeatherCache.length();index++) {
-    locationWeatherCache.getWeatherAtIndexForDate(index, new Date(), function(index, data){
-    var summary = 'Min: ' + data.temperatureMin + ', Max: ' + data.temperatureMax;
-    document.getElementById('weather' + index).textContent = summary;
-    document.getElementById('icon' + index).src = 'images/' + data.icon + '.png';
-    });
-   }
+        locationWeatherCache.getWeatherAtIndexForDate(index, new Date(), function(index, data){
+        var summary = 'Min: ' + data.temperatureMin + ', Max: ' + data.temperatureMax;
+        // Displaying weather summary
+        document.getElementById('weather' + index).textContent = summary; 
+        // Displaying weather icon
+        document.getElementById('icon' + index).src = 'images/' + data.icon + '.png';
+       });
+    }
 }
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
