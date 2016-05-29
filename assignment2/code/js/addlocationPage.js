049f048404c392ec5cb1451b6adf2ac9fba95c22
nmjon1 <nmjon1@student.monash.edu>
2016-05-29 15:01:25 +1000

    Minor Adjustments prior to Pull

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -63,3 +63,10 @@ function initMap() {
   });
 
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+}
