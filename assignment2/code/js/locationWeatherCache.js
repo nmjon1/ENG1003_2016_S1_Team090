8ec541050a7df1343e732fc516f782ac3fd27841
Jarred Gallina <jarred.gallina@qxbranch.com>
2016-05-29 16:12:38 +1000

    Commenting, and some cleanup

--- assignment2/code/js/locationWeatherCache.js
+++ assignment2/code/js/locationWeatherCache.js
@@ -32,6 +32,9 @@ function LocationWeatherCache()
 
     var locations = [];
     var callbacks = {};
+
+    // We create a variable to store a reference ot this object, so
+    // that we can access it from within private functions.
     var me = this;
 
     // Public methods:
@@ -55,8 +58,11 @@ function LocationWeatherCache()
     //
     this.addLocation = function(latitude, longitude, nickname)
     {
+        // We check if this location is already stored.
+        // indexForLocation returns null if it is not.
         var index = indexForLocation(latitude,longitude);
 
+        // If the location isn't stored, we add it.
         if (index == null) {
             locations.push({
                 nickname: nickname,
@@ -64,9 +70,16 @@ function LocationWeatherCache()
                 longitude: longitude,
                 forecasts: {}
             });
-            saveLocations()
+
+            // We must save the locations to local storage every
+            // time we make a change.
+            saveLocations();
+
             return locations.length - 1
+
         } else {
+            // If the location is already stored, we return its
+            // index.
             return index;
         }
     };
@@ -79,6 +92,8 @@ function LocationWeatherCache()
         saveLocations();
     };
 
+    // Removes every stored location.
+    // Mostly for testing, can't be accessed from the user interface.
     this.removeAllLocations = function() {
         locations.splice(0,locations.length);
         saveLocations()
@@ -89,11 +104,16 @@ function LocationWeatherCache()
     // are active web service requests and so doesn't need to be saved.
     //
     this.toJSON = function() {
+
+        // Creates an array to store the strinfigied locations.
         var json = [];
+
+        // We loops through every location, stringifying and storing.
         for (var i = 0; i < locations.length; i++) {
             json.push(JSON.stringify(locations[i]))
         }
 
+        // Stringifies the collection of stringified locations.
         return JSON.stringify(json)
     };
 
@@ -102,7 +122,12 @@ function LocationWeatherCache()
     // instance to match that version.
     //
     this.initialiseFromPDO = function(locationWeatherCachePDO) {
+
+        // Gets the list of stringified locations.
         var json_list = JSON.parse(locationWeatherCachePDO);
+
+        // Loops through the locations and parses them, then
+        // pushes to the location list.
         for (var i = 0; i < json_list.length; i++) {
             var location = JSON.parse(json_list[i]);
             locations.push(location)
@@ -120,27 +145,41 @@ function LocationWeatherCache()
     // weather object for that location.
     // 
     this.getWeatherAtIndexForDate = function(index, date, callback) {
+
+        // This API key is the one linked to the team's account.
         var APIKEY = 'b0d9cfc6e50e108064070318f45d3254';
 
+        // We get the location for the given index and build the
+        // forecast key, this is used to store forecasts in each
+        // location object, as well as callback functions in the
+        // callback object. We also use it in the API call.
         var location = this.locationAtIndex(index);
         var forecast_key = String(location.latitude) + ','
             + String(location.longitude) + ','
             + date.forecastDateString();
+
+        // We initialise a variable to store the forecast info.
         var forecast;
 
+        // We store the callback function, in case we need to use
+        // it later on. This will be needed if the forecast isn't
+        // already cached.
         callbacks[forecast_key] = callback;
 
+        // If we have the forecast stored, we run the callback
+        // function here. Otherwise, we get it from the API.
         if (location.forecasts.hasOwnProperty(forecast_key)) {
             callback(index, location.forecasts[forecast_key])
         } else {
-            // Get it from the API
+            // We have to build the API call and append it to
+            // the HTML document.
             var script = document.createElement("script");
-            script.src = 'https://api.forecast.io/forecast/'
-                + APIKEY + '/'
-                + forecast_key
-                + '?callback=locationWeatherCache.weatherResponse'
-                + '&exclude=hourly,minutely,currently'
-                + '&units=ca';
+            script.src = 'https://api.forecast.io/forecast/' // The base of the API call
+                + APIKEY + '/' // Our API key
+                + forecast_key // The forecast key, as built above.
+                + '?callback=locationWeatherCache.weatherResponse' // weatherResponse is called by the API when the forecast is ready
+                + '&exclude=hourly,minutely,currently' // We exclude information we don't care about to save time and space
+                + '&units=ca'; // We set our units to SI
             document.body.appendChild(script);
         }
 
@@ -150,22 +189,31 @@ function LocationWeatherCache()
     // This is a callback function passed to forecast.io API calls.
     // This will be called via JSONP when the API call is loaded.
     //
-    // This should invoke the recorded callback function for that
-    // weather request.
+    // This stores the response and invokes the recorded callback
+    // function for that weather request.
     //
-
-
     this.weatherResponse = function(response) {
+
+        // We get the daily data for the day we care about
         var today =  response.daily.data[0];
+
+        // We gather all the relevant information for storage.
         var index = indexForLocation(response.latitude,response.longitude);
         var location = this.locationAtIndex(index);
-        var date = new Date(today.time * 1000);
+        var date = new Date(today.time * 1000); // The time is given in seconds, so we multiply by 1000 to convert to milliseconds.
+
+
+        // We build forecast key, this is used to store the forecast
+        // and get the callback function.
         var forecast_key = String(response.latitude) + ','
             + String(response.longitude) + ','
             + date.forecastDateString();
+
+        // We store the forecast and save  to local storage.
         location.forecasts[forecast_key] = today;
         saveLocations();
 
+        // We run the callback function on the data.
         var callback = callbacks[forecast_key];
         callback(index,today)
 
@@ -177,7 +225,7 @@ function LocationWeatherCache()
     // Given a latitude and longitude, this method looks through all
     // the stored locations and returns the index of the location with
     // matching latitude and longitude if one exists, otherwise it
-    // returns -1.
+    // returns null.
     //
     function indexForLocation(latitude, longitude)
     {
@@ -192,12 +240,9 @@ function LocationWeatherCache()
 
 // Restore the singleton locationWeatherCache from Local Storage.
 //
-
-var locationWeatherCache = new LocationWeatherCache;
-loadLocations();
-
 function loadLocations()
 {
+    // We check that the locations are actually stored, then initialise.
     if (!(localStorage.getItem(APP_PREFIX + '-locations') === null)) {
         var lwc_PDO = localStorage.getItem(APP_PREFIX + '-locations');
         locationWeatherCache.initialiseFromPDO(lwc_PDO);
@@ -205,13 +250,13 @@ function loadLocations()
 }
 
 // Save the singleton locationWeatherCache to Local Storage.
-//
+// We run this function every time the locations object is changed.
 function saveLocations()
 {
+    // We save the locations object to local storage.
     localStorage.setItem(APP_PREFIX + "-locations", locationWeatherCache.toJSON())
 }
 
-// Testing stuff
-
-function my_return(index, thing) {return thing}
-
+// We create the cache and attempt to load data from local storage.
+var locationWeatherCache = new LocationWeatherCache;
+loadLocations();
